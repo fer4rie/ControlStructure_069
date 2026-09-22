d1191c43b28a9e88bde769a00ce61574fa089690
fer4rie <firapuspita2106@gmail.com>
2026-09-22 11:01:33 +0700

    number 1: evaluate the student performance

---
+++ controlstructure1.py
@@ -0,0 +1,14 @@
+# 1. Write a PYTHON program to evaluate the student performance
+
+student_grade = int(input("Enter student performance (0-100) :"))
+
+if student_grade >=90:
+    print("Excellent performance")
+elif student_grade >=80:
+    print("Very good performance")
+elif student_grade >=70:
+    print("Good performance")
+elif student_grade >=60:
+    print("Average performance")
+else :
+    print ("Poor performance") 
